1355687d47cb8ba7a6e6d333dcb215db90f8828b
Emre <emrebadem_@hotmail.com>
2022-10-26 05:33:39 +0300

    feat: add swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Set up swagger
+  const config = new DocumentBuilder()
+    .setTitle('University Exam Simulation')
+    .setDescription('An app to simulate University exams')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+
 }
 bootstrap();
